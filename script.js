a6ddfcc309e51bfb3402ebad3e169a1565b55fb5
moi24py <moi24py@gmail.com>
2026-08-30 14:37:24 +0200

    Add function to randomly return a string 'rock', 'paper', 'scissors'

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+// Randomly returns a string value: “rock”, “paper” or “scissors”
+function getComputerChoice(){
+    let rand = Math.floor((Math.random() * 3));
+    let pick;
+    if (rand === 0) pick = "rock";
+    else if (rand === 1) pick = "paper";
+    else pick = "scissors";
+    return pick;
+}
+
+getComputerChoice();
