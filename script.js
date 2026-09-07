9086895898bc6edd82c19e13daee20f434112df5
moi24py <moi24py@gmail.com>
2026-09-07 16:17:22 +0200

    Add a node to show the current round winner

--- script.js
+++ script.js
@@ -24,3 +24,8 @@ const scissors = document.querySelector("#scissors");
 // Show a button with the computer choice
 const computerShowBtn = document.createElement("button");
 computerShowBtn.setAttribute("style", "padding: 8px; width: 85px; border-radius: 5px; border: 1px solid rgb(250, 80, 38); background-color: rgb(255, 60, 21); text-transform: uppercase; font-weight: 600; text-align: center; box-shadow: 3px 3px rgb(255, 60, 21);");
+
+// Show the winner of the current round
+const matchDiv = document.createElement("div");
+matchDiv.classList.add(".match-div");
+matchDiv.setAttribute("style", "display:flex; align-items: center; justify-content: center; width: 90%; height: 100px; background-color:rgb(1,49,88); margin: 0 auto; text-align: center; text-transform: uppercase; color: rgb(240, 197, 88); font-size: xx-large;");
